377a0af96c26fd15c516fc4d63915e6c44f067ee
gsr142 <gsr142@gmail.com>
2023-07-28 15:18:40 -0700

    add comments, clean up code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Use require to import the necessary functions and inputs.
 const inquirer = require('inquirer')
 const questions = require('./lib/questions.js');
 const generateLogo = require('./lib/generateLogo.js');
-
+// Prompts the user with questions, then calls generateLogo() with the argument 'input'.
+// input is the user responses to the prompted questions.
 inquirer
     .prompt(questions)
     .then((input) => {
